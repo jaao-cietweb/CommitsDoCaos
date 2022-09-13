c9ce1503489e590e208471745efeb6be3f952ac2
joaovg@ciandt.com <joaovg@ciandt.com>
2022-09-13 14:53:01 -0300

    00 Starting development

---
+++ Program.java
@@ -0,0 +1,13 @@
+public class Program {
+    public static void main(String[] args) {
+        calculadora calculadora= new calculadora();
+
+        System.out.println("Dado os valores 15 e 30 o resultado das seguintes operações são:");
+        System.out.println("_______________________________________________________________");
+        System.out.println("Soma: "+ calculadora.soma(15, 30));
+        System.out.println("Subtração: "+ calculadora.subtracao(15, 30));
+        System.out.println("Multiplicação: " + calculadora.multiplicacao(15, 30));
+        System.out.println("Divisão: "+ calculadora.divisao(15, 30));
+
+    }
+}
---
+++ calculadora.java
@@ -0,0 +1,14 @@
+public class calculadora {
+    public int soma(int x, int y){
+        return 0;
+    }
+    public int subtracao(int x, int y){
+        return 0;
+    }
+    public int multiplicacao(int x, int y){
+        return 0;
+    }
+    public int divisao(int x, int y){
+        return 0;
+    }
+}
